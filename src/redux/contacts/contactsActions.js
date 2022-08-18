a9c75d2ba46e421de072a79ac909e7c1b77cd745
GubarievaAnna <annagubarieva@gmail.com>
2022-08-18 23:28:04 +0300

    +

--- src/redux/contacts/contactsActions.js
+++ src/redux/contacts/contactsActions.js
@@ -3,3 +3,35 @@ import { createAction } from '@reduxjs/toolkit';
 export const addContact = createAction('contacts/add');
 export const deleteContact = createAction('contacts/delete');
 export const filterContacts = createAction('contacts/changeFilter');
+
+// import { createSlice } from '@reduxjs/toolkit';
+
+// const todoSlice = createSlice({
+//   name: 'todo',
+//   initialState: {
+//     items: [],
+//     filter: 'all',
+//     // isLoading: false,
+//   },
+//   reducers: {
+//     addTodo(state, { payload }) {
+//       return { ...state, items: [...state.items, payload] };
+//       //   state.items.push(payload);
+//     },
+//     removeTodo(state, { payload }) {
+//       return { ...state, items: state.items.filter(el => el.id !== payload) };
+//     },
+//     updateStatusTodo(state, { payload }) {
+//       state.items = state.items.map(el =>
+//         el.id !== payload ? el : { ...el, isDoneStatus: !el.isDoneStatus }
+//       );
+//     },
+//     changeFilter(state, { payload }) {
+//       state.filter = payload;
+//     },
+//   },
+// });
+
+// export const { addTodo, removeTodo, updateStatusTodo, changeFilter } =
+//   todoSlice.actions;
+// export default todoSlice.reducer;
